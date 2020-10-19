2bcb8e2faa2adc143c9ab50bf252d45c09549374
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2020-10-18 21:01:05 -0300

    Initial project add

---
+++ src/main/java/rbf/springframework/rbfpetclinic/RbfPetClinicApplication.java
@@ -0,0 +1,13 @@
+package rbf.springframework.rbfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbfPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbfPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rbf/springframework/rbfpetclinic/RbfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package rbf.springframework.rbfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbfPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
