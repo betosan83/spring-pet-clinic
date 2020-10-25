5258673dfa37679bda3b7626af5e2d16210d251a
Roberto Boscolo Figueredo <33357673+betosan83@users.noreply.github.com>
2020-10-24 22:45:28 -0300

    Added Vets list and updated controller. Closes #15.

--- pet-clinic-web/src/main/java/rbf/springframework/rbfpetclinic/controlers/VetController.java
+++ pet-clinic-web/src/main/java/rbf/springframework/rbfpetclinic/controlers/VetController.java
@@ -1,14 +1,25 @@
 package rbf.springframework.rbfpetclinic.controlers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import rbf.springframework.rbfpetclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
